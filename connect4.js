370dc154fafdc268b240e934917ba7077da60065
obieglok <obieglok@tcd.ie>
2020-06-11 13:00:58 +0100

    Set up event listener to show what column we are pointing at with the mouse

--- connect4.js
+++ connect4.js
@@ -4,6 +4,7 @@ class Connect4{
     this.COLUMNS=7;
     this.selector=selector;
     this.createGrid();
+    this.setupEventListeners();
   }
   //$ is shortcut for jquery
   createGrid(){
@@ -15,11 +16,40 @@ class Connect4{
 
       for(let column=0; column<this.COLUMNS; column++)
       {
-        const $column=$('<div>').addClass('column empty');//create columnds
+
+        const $column=$('<div>').addClass('column empty').attr("data-column",column).attr("data-row",row);//create columns and give them values
 
         $row.append($column); //appends the columns to each row, so each row has 7 columns
       }
       $board.append($row); //add rows(with the appeneded columns) to the board
     }
+  }
+  setupEventListeners() {
+
+    const $board=$(this.selector);
+    function findLastEmptyCell(column){
+      // finds all the cells in the column that are empty
+      const cells= $(`.column[data-column='${column}']`);
+      for(let i=cells.length-1; i>=0; i--)
+      {
+        const $cell=$(cells[i]);
+        if($cell.hasClass('empty'))
+        {
+          return $cell;
+        }
+      }
+      return nulll;
+    }
+    // JQuery will listen to mouse entering a column
+    $board.on("mouseenter",".column.empty",function(){
+      const column=$(this).data('column');
+      const $lastEmptyCell=findLastEmptyCell(column);
+      $lastEmptyCell.addClass('next-red')
+    //  console.log(column);
+    })
+
+    $board.on('mouseleave', '.column',function(){
+      $('.column').removeClass('next-red');
+    })
   }
 }
